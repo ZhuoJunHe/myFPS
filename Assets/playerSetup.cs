80ddec4bce6e6b0b1e409a361e1c5e96a09fff4f
ZhuojunHe <hezhuojun@yahoo.com>
2020-06-01 22:32:40 -0700

    my fps game
    first commit

---
+++ Assets/playerSetup.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+
+
+public class playerSetup : NetworkBehaviour
+{
+	[SerializeField]
+	Behaviour[] compDisable;
+
+	Camera sceneCamera;
+
+	void Start()
+	{
+		if (!isLocalPlayer)			
+		{
+			for (int i=0; i < compDisable.Length; i++)
+			    {
+				compDisable[i].enabled = false;
+				
+			     }
+
+		}
+	}
+}
